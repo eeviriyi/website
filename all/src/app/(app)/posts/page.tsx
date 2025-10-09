ae90d330332fcf7e5a9d3093f6005332c163f912
Eeviriyi <eeviriyi@outlook.com>
2025-10-09 21:22:44 +0800

    Remove time page

--- all/src/app/(app)/posts/page.tsx
+++
@@ -1,7 +0,0 @@
-import PostsList from "@/components/homepage/PostsList";
-import { getPostsByDate } from "@/lib/posts/PostsUtils";
-
-export default async function PostsPage() {
-  const posts = await getPostsByDate();
-  return <PostsList posts={posts} />;
-}
--- all/src/lib/ai/send-notification.ts
+++
@@ -1,6 +0,0 @@
-import { scSend } from "serverchan-sdk";
-
-export const sendNotification = async (title: string, desp: string) => {
-  const sendkey = "SCT296379T1nEubWuyuFJ7YyWKZWAtbtbq";
-  await scSend(sendkey, title, desp, { tags: "服务器报警|报告" });
-};
